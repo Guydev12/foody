cd382413ea313c35d09e4afb3e5bfd8007166f86
Guivens Joseph <josephguivens23@gmail.com>
2024-10-22 18:20:43 +0000

    implements the sliceshow image in the header

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
+
 import classes from "./page.module.css";
+import { ImageSlideshow } from "@/components/images/image-sliceshow";
+
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div classNane={classes.sliceshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
-            <h1>NextLevel food for next level Foody</h1>
-            <p>taste & share food from all the world.</p>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
             <Link href="/community">Join the Community</Link>
